8f81645783388c1237faa698dc87e902e36f868a
KonstantinOsenov <84586750+KonstantinOsenov@users.noreply.github.com>
2026-05-16 20:27:26 +0200

    Update python_help.py

--- python_help.py
+++ python_help.py
@@ -100,7 +100,116 @@ a.zfill() 			# Fills the string with a specified number of 0 values at the begin
 #################################
 #### Python Lists
 #################################
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Change the second item:
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+# Change the values "banana" and "cherry" with the values "blackcurrant" and "watermelon":
+# If you insert more items than you replace, the new items will be inserted where you specified, and the remaining items will move accordingly
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+# Insert "watermelon" as the third item:
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
 
+# Using the append() method to append an item:
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+# Add the elements of tropical to thislist:
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+# Remove "banana":
+# If there are more than one item with the specified value, the remove() method removes the first occurrence:
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+# Remove the second item:
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+# If you do not specify the index, the pop() method removes the last item.
+
+# Remove the first item:
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+# List Comprehension
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if "a" in x]
+print(newlist)
+newlist = [x if x != "banana" else "orange" for x in fruits]
+
+# Sort the list alphabetically:
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+thislist.sort(reverse = True)
+print(thislist)
+
+# Customize Sort Function
+def myfunc(n):
+  return abs(n - 50)
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+# By default the sort() method is case sensitive, resulting in all capital letters being sorted before lower case letters:
+thislist.sort(key = str.lower)
+
+# Make a copy of a list with the copy() method:
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+mylist = list(thislist) # list method
+mylist = thislist[:] # slice operator
+
+# Join Two Lists
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+list3 = list1 + list2
+print(list3)
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+for x in list2:
+  list1.append(x)
+print(list1)
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+list1.extend(list2)
+print(list1)
+
+# List Methods
+# append()		Adds an element at the end of the list
+# clear()		Removes all the elements from the list
+# copy()		Returns a copy of the list
+# count()		Returns the number of elements with the specified value
+# extend()		Add the elements of a list (or any iterable), to the end of the current list
+# index()		Returns the index of the first element with the specified value
+# insert()		Adds an element at the specified position
+# pop()			Removes the element at the specified position
+# remove()		Removes the item with the specified value
+# reverse()		Reverses the order of the list
+# sort()		Sorts the list
+
+#################################
+#### Python Tuples
+#################################
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+
+
+
+# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
 
 
 
