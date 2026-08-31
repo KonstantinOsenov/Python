16ff0c0c98f74c827bef71c8005bcfce54a7f1cb
KonstantinOsenov <84586750+KonstantinOsenov@users.noreply.github.com>
2026-08-31 16:14:41 +0200

    Create Automated tasks windows.py

---
+++ Automated tasks windows.py
@@ -0,0 +1,25 @@
+"""
+# Automated tasks windows.txt
+"C:\Users\A949424\Documents\experiment\run_test.bat"
+"C:\Users\A949424\Anaconda3\python.exe" "C:\Users\A949424\Documents\experiment\test_schedule.py"
+
+-- backup
+run_test.bat
+
+C:\Users\A949424\Documents\experiment
+./run_test.bat
+
+C:\Users\A949424\Documents\experiment/run_test.bat
+
+CD /d %~dp0
+Call "C:\Users\A949424\Anaconda3\python.exe" "C:\Users\A949424\Documents\experiment\test_schedule.py"
+
+# run_test.bat
+"C:\Users\A949424\Anaconda3\python.exe" "C:\Users\A949424\Documents\experiment\test_schedule.py"
+
+# test_schedule.py
+import webbrowser
+
+webbrowser.open('https://github.com/KonstantinOsenov/Python')
+
+"""
