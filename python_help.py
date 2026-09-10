d4887bb09ce581ef378d64d6cc93280f995dcf19
KonstantinOsenov <84586750+KonstantinOsenov@users.noreply.github.com>
2026-09-10 17:38:39 +0200

    Update python_help.py

--- python_help.py
+++ python_help.py
@@ -1,4 +1,13 @@
 #################################
+## regexp
+#################################
+import re
+#Check if the string starts with "The" and ends with "Spain":
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+x
+
+#################################
 ## variables
 #################################
 x = "Cherry"
